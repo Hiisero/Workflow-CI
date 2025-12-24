ad40856bade5296cf34d836328281773332f9e5b
MrTa <76782241+Hiisero@users.noreply.github.com>
2025-12-24 17:39:21 +0700

    Update modelling_tuning.py

--- MLProject/modelling_tuning.py
+++ MLProject/modelling_tuning.py
@@ -8,7 +8,7 @@ import mlflow.sklearn
 
 def main():
     # mlflow.set_tracking_uri("http://127.0.0.1:5000")
-    mlflow.set_experiment("Heart Disease Classification - Tuning V2")
+    # mlflow.set_experiment("Heart Disease Classification - Tuning V2")
     
     df = pd.read_csv("heart_preprocessing.csv")
 
@@ -38,44 +38,44 @@ def main():
         scoring="f1"
     )
 
-    with mlflow.start_run():
-        # Train
-        grid_search.fit(X_train, y_train)
-        best_model = grid_search.best_estimator_
+    # Train
+    grid_search.fit(X_train, y_train)
+    best_model = grid_search.best_estimator_
 
-        # Predict
-        y_pred = best_model.predict(X_test)
+    # Predict
+    y_pred = best_model.predict(X_test)
 
-        # Metrics
-        accuracy = accuracy_score(y_test, y_pred)
-        precision = precision_score(y_test, y_pred)
-        recall = recall_score(y_test, y_pred)
-        f1 = f1_score(y_test, y_pred)
+    # Metrics
+    accuracy = accuracy_score(y_test, y_pred)
+    precision = precision_score(y_test, y_pred)
+    recall = recall_score(y_test, y_pred)
+    f1 = f1_score(y_test, y_pred)
 
-        # Manual logging
-        mlflow.log_metric("accuracy", accuracy)
-        mlflow.log_metric("precision", precision)
-        mlflow.log_metric("recall", recall)
-        mlflow.log_metric("f1_score", f1)
+    # Manual logging
+    mlflow.log_metric("accuracy", accuracy)
+    mlflow.log_metric("precision", precision)
+    mlflow.log_metric("recall", recall)
+    mlflow.log_metric("f1_score", f1)
 
-        mlflow.log_param("best_C", grid_search.best_params_["C"])
-        mlflow.log_param("best_solver", grid_search.best_params_["solver"])
-        mlflow.log_param("cv_folds", 5)
-        mlflow.log_param("model_type", "LogisticRegression")
+    mlflow.log_param("best_C", grid_search.best_params_["C"])
+    mlflow.log_param("best_solver", grid_search.best_params_["solver"])
+    mlflow.log_param("cv_folds", 5)
+    mlflow.log_param("model_type", "LogisticRegression")
 
-        # Log model artifact
-        mlflow.sklearn.log_model(
-            sk_model=best_model,
-            artifact_path="model"
-        )
+    # Log model artifact
+    mlflow.sklearn.log_model(
+        sk_model=best_model,
+        artifact_path="model"
+    )
 
-        print("Best Parameters :", grid_search.best_params_)
-        print("Accuracy        :", accuracy)
-        print("Precision       :", precision)
-        print("Recall          :", recall)
-        print("F1-score        :", f1)
+    print("Best Parameters :", grid_search.best_params_)
+    print("Accuracy        :", accuracy)
+    print("Precision       :", precision)
+    print("Recall          :", recall)
+    print("F1-score        :", f1)
 
 
 if __name__ == "__main__":
     main()
+
 
